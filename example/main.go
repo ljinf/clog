20b2d0816d4aa7f35166270160c9c2aaf8bf7f11
ljf <1945780820@qq.com>
2023-08-16 18:59:44 +0800

    edit

--- example/main.go
+++ example/main.go
@@ -1,23 +1,29 @@
 package main
 
+import (
+	"github.com/ljinf/clog"
+	"log"
+	"os"
+)
+
 func main() {
-	//clog.Info("std log")
-	//clog.SetOptions(clog.WithLevel(clog.DebugLevel))
-	//clog.Debug("change std log to debug level")
-	//
-	////输出到文件
-	//file, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	//if err != nil {
-	//	log.Fatal("create file test.log failed")
-	//	return
-	//}
-	//defer file.Close()
-	//
-	//logger := clog.New(
-	//	clog.WithLevel(clog.InfoLevel),
-	//	clog.WithOutput(file),
-	//	clog.WithFormatter(&clog.JsonFormatter{IgnoreBasicFields: false}),
-	//)
-	//logger.Debug("debug log")
-	//logger.Info("custom log with json formatter")
+	clog.Info("std log")
+	clog.SetOptions(clog.WithLevel(clog.DebugLevel))
+	clog.Debug("change std log to debug level")
+
+	//输出到文件
+	file, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("create file test.log failed")
+		return
+	}
+	defer file.Close()
+
+	logger := clog.New(
+		clog.WithLevel(clog.InfoLevel),
+		clog.WithOutput(file),
+		clog.WithFormatter(&clog.JsonFormatter{IgnoreBasicFields: false}),
+	)
+	logger.Debug("debug log")
+	logger.Info("custom log with json formatter")
 }
